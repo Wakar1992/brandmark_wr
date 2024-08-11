acdf505fa1266ce396767dd3fdd93f1d2c80a848
wakar1992 <wakar786ahsan@gmail.com>
2024-08-12 02:26:03 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <img src="https://brandmark.io/images/logo.svg" alt="" srcset="" />
+    </div>
+  )
+}
+
+export default App
