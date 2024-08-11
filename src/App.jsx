3e493f8eb4b08ec0b2f57872177f6f349c3c32e4
wakar1992 <wakar786ahsan@gmail.com>
2024-08-12 02:35:49 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Tools from './pages/Tools'
+
 
 function App() {
   return (
     <div>
       <img src="https://brandmark.io/images/logo.svg" alt="" srcset="" />
+      <Tools/>
     </div>
   )
 }
