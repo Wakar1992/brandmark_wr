cbbee162971407a7ff99b5a228498e1c0acd7f56
wakar1992 <wakar786ahsan@gmail.com>
2024-12-22 23:53:47 +0530

    header changes

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -32,7 +32,7 @@ function Header() {
   ]
   return (
     <div>
-      <a className='text-white text-xl font-bold justify-center items-start' href="tel:+919166538689">+919166538689</a>
+      <a className='text-white text-xl font-bold justify-center items-start' href="tel:+919166538689"> +919166538689</a>
       <nav className='w-full flex bg-white justify-between items-center gap-1 lg:px-16 px-6 py-4 sticky top-0 z-50'>
 
       </nav>
